30517a10a748172ee367157281ff60141ad10830
Colin Hoernig <Hoernig.Colin@gmail.com>
2016-03-19 00:00:53 -0500

    Reimplement test_helpers.js from scratch as a learning exercise.

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,35 +1,39 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
+import jQuery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
 import chai, { expect } from 'chai';
 import chaiJquery from 'chai-jquery';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
 
+// Set up testing env to run like a browser, but in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
-const $ = _$(window);
+const $ = jQuery(global.window);
 
+// Set up chai-jquery
 chaiJquery(chai, chai.util, $);
 
+// Build renderComponent helper that should render a given React class
 function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
+  const componentInstance = TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
     </Provider>
   );
 
-  return $(ReactDOM.findDOMNode(componentInstance));
+  return $(ReactDOM.findDOMNode(componentInstance)); // get the component HTML
 }
 
+// Build a helper for simulating events
 $.fn.simulate = function(eventName, value) {
   if (value) {
     this.val(value);
   }
   TestUtils.Simulate[eventName](this[0]);
-};
+}
 
-export {renderComponent, expect};
+export { renderComponent, expect };
